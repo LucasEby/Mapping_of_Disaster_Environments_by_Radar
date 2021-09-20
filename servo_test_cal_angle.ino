83af0a7ea31977702b3dde3488c913b4e4ab4e53
Aderland <50410999+Aderland@users.noreply.github.com>
2021-09-20 11:39:53 -0400

    Trigger and output angles working

--- servo_test_cal_angle.ino
+++ servo_test_cal_angle.ino
@@ -1,4 +1,4 @@
-#inlcude <Servo.h>
+#include <Servo.h>
 
 Servo servo_h;
 Servo servo_v;
@@ -6,37 +6,43 @@ Servo servo_v;
 int angle_h = 0;
 int angle_v = 0;
 int servoDirection = 1;
+int x;
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); // set up the Serial Library at 9600 bps 
-  servo_h.attach(9); // servo for horizontal movement connected to pin 9
-  servo_v.attach(10); // servo for vertical movement connected to pin 10
+  Serial.begin(115200); // set up the Serial Library at 9600 bps 
+  Serial.setTimeout(1);
+  // servo_h.attach(9); // servo for horizontal movement connected to pin 9
+  // servo_v.attach(10); // servo for vertical movement connected to pin 10
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(Serial.available()) {
-    // change the position of the servo when the PC instructs
-    // how to stop the loop when scanning finished??? 
-    // exit(0) could stop the loop
-    servo_h.write(angle_h);
-    servo_v.write(angle_v);
+  while (!Serial.available());
+  x = Serial.readString().toInt();
 
-    // MAKE THE SCAN AND GET THE RESULT FROM THE RADAR
+  // the motor set to be trigger when intputted 5
+  if (x == 5) {
+    Serial.println("Motor started:");
+    while (angle_v < 180) {
+      Serial.print("H: ");
+      Serial.print(angle_h);
+      Serial.print("; V: ");
+      Serial.println(angle_v);
+      
+      if (servoDirection == 1)
+        angle_h++;
+      else
+        angle_h--;
 
-    // check the horizontal scanning direction
-    if (servoDirection == 1)
-      angle_h++;
-    else
-      angle_h--;
-
-    // Change the direction when edges is reached
-    if (angle_h == 180 || servoRotatePosition == 0) {
-      servoDirection = -servoDirection;
-      angle_h = angle_h%181;
-      angle_v++;
+      if (angle_h == 180 || angle_h == 0) {
+        servoDirection = -servoDirection;
+        angle_h = angle_h % 181;
+        angle_v++;
+      }
     }
+  } else {
+    Serial.println("Not triggered");
   }
 }
