c1f79606eba93a4f78d4f7ea03db2239c918f985
gajjara <gajjar.a@husky.neu.edu>
2021-11-07 16:44:09 -0500

    changed file names

--- process_example.py
+++ process_example.py
@@ -1,27 +1,24 @@
 # Standard Library Imports
-from queue import Queue, Empty
+from multiprocessing import Queue
+from time import sleep
 
 # Package Imports
 import numpy as np
 import matplotlib.pyplot as plot
 
 # Self Imports
-from data import Reader
-from control import Control, Ports
-from threads import ReadThread
+from control import Ports, Control
+from processes import SerialProcess
 
 if __name__ == '__main__':
-    #config_file_name = 'xwr68xx_profile_2021_10_24T03_23_44_468.cfg'
-    config_file_name = 'xwr68xx_profile_2021_11_06T20_15_26_698.cfg'
-    #config_file_name = '/Users/Anuj/Downloads/profile_2021_11_06T23_37_39_285.cfg'
-    ports = Ports(attach_time=30.0)
-    reader = Reader(ports)
-    controller = Control(config_file_name, ports)
-
+    config_file_name = 'xwr68xx_profile_2021_10_24T03_23_44_468.cfg'
     queue = Queue()
-    thread1 = ReadThread(reader, queue)
-    thread1.start()
-
+    ports = Ports(attach_time=20.0, attach_to_ports=False)
+    process = SerialProcess(queue, ports.data_port, 921600, 0.1)
+    control = Control(config_file_name, ports)
+    sleep(0.1)
+    process.start()
+    
     fig = plot.figure()
     axis = fig.add_subplot(projection='3d')
     xs = [0.0]
@@ -39,24 +36,26 @@ if __name__ == '__main__':
     fig.colorbar(sp, label="SNR")
 
     while True:
-        try:
-            while not(queue.empty()):
-                gotten = queue.get(block=True)
-                for got in gotten:
-                    xs.append(got.x)
-                    ys.append(got.y)
-                    zs.append(got.z)
-                    cs.append(got.snr)
-                sp._offsets3d = (np.array(xs),np.array(ys),np.array(zs))
-                cs_array = np.array(cs)
-                sp.set_array(cs_array)
-                sp.set_clim(min(cs), np.quantile(cs_array, 0.8))
-                axis.set_xlim([min(xs), max(xs)])
-                axis.set_ylim([min(ys), max(ys)])
-                axis.set_zlim([min(zs), max(zs)])
-                fig.canvas.draw_idle()
-                plot.show(block=False)
-                plot.pause(0.01)
+        while not(queue.empty()):
+            gotten = queue.get(block=True)
+            for got in gotten:
+                xs.append(got.x)
+                ys.append(got.y)
+                zs.append(got.z)
+                cs.append(got.snr)
+            sp._offsets3d = (np.array(xs),np.array(ys),np.array(zs))
+            cs_array = np.array(cs)
+            sp.set_array(cs_array)
+            sp.set_clim(min(cs), np.quantile(cs_array, 0.8))
+            axis.set_xlim([min(xs), max(xs)])
+            axis.set_ylim([min(ys), max(ys)])
+            axis.set_zlim([min(zs), max(zs)])
+            fig.canvas.draw_idle()
+            plot.show(block=False)
             plot.pause(0.01)
-        except Empty:
-            pass
+        plot.pause(0.01)
+    #while True:
+    #    if not(queue.empty()):
+    #        print(queue.get())
+    #    sleep(0.01)
+    
